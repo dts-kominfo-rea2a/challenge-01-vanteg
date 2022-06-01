c9919872f002deb671c2933839861b973a1c7566
Irvan Wijaya <irvanwijaya72@gmail.com>
2022-06-01 13:24:24 +0700

    mengubah users menjadi let users new set

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-let favoriteColor1=new Set (["Yellow", "Pink", "White", "Purple"]);
-let favoriteRestaurant1= new Set (["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]);
-let favoriteColor2=new Set (["Blue", "Black", "Grey"]);
-let favoriteRestaurant2=new Set (["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]);
-const firstUser = {
+let favoriteColor1=["Yellow", "Pink", "White", "Purple"];
+let favoriteRestaurant1= ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"];
+let favoriteColor2=["Blue", "Black", "Grey"];
+let favoriteRestaurant2=["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"];
+let firstUser = {
   name: "Monica",
   gender: "Female",
   age: "17",
   email: "monica@dingdong.com",
-  favoriteColor : favoriteColor1,
+  favoriteColor : new Set(favoriteColor1),
   isHavePet : "Yes",
   education : [{name: "SD 01", city: "Jakarta", graduate: 2016},{name: "SMP 02", city: "Jakarta", graduate: 2019},{name: "SMA 03", city: "Tangerang"}],
-  favoriteRestaurant :favoriteRestaurant1,
+  favoriteRestaurant : new Set(favoriteRestaurant1),
 };
-const secondUser = {
+let secondUser = {
   name: "Wendy",
   gender: "Male",
   age: "23",
   email: "wendy@dingdong.com",
-  favoriteColor : favoriteColor2,
+  favoriteColor : new Set(favoriteColor2),
   isHavePet : "No",
   education : [{name: "SD 02", city: "Jakarta", graduate: 2010},{name: "SMP 03", city: "Bogor", graduate: 2013},{name: "SMA 01", city: "Surabaya"},{name: "Universitas Maju", city: "Tangerang"}],
-  favoriteRestaurant : favoriteRestaurant2,
+  favoriteRestaurant : new Set(favoriteRestaurant2),
 
 }
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-let users = [(firstUser),(secondUser)];
+let users= new Set();
+users.add(firstUser);
+users.add(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
     console.log(users.length || users.size);
     console.log(users);
-}
+    // console.log(users[1].favoriteRestaurant1.length);
+  }
 
 main();
 
